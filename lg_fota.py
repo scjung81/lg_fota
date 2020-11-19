fc13ec9bd59ab2b38f1913093538628fd15540ef
automation pc <jungil.kwon@sk.com>
2020-11-19 17:55:11 +0900

    add comment

--- lg_fota.py
+++ lg_fota.py
@@ -1,4 +1,4 @@
-#git에소 추가1
+# [20201119] git에소 추가
 
 from lg_fota_get_data_from_mail import lg_fota_get_data
 from lg_fota_send_mail import lg_fota_send_mail
@@ -11,5 +11,3 @@ lg_fota_send_mail()
 
 from sendMail import *
 sendMail(title="lg_fota end ", text="")
-
-#test
