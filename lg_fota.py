609d3c42b0f6bfd05056a9171fe857a51280ee98
jungil_176 <jungil.kwon@sk.com>
2020-11-19 18:12:05 +0900

    git test4

--- lg_fota.py
+++ lg_fota.py
@@ -1,5 +1,6 @@
 # [20201119] git에소 추가
-# 개인 PC에서 주석 추가 
+# 167 PC에서 commit 추가
+# 개인 PC에서 주석 추가
 
 from lg_fota_get_data_from_mail import lg_fota_get_data
 from lg_fota_send_mail import lg_fota_send_mail
