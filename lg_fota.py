f360002dba3c38de95a8396a0fdd7104f3706dbf
automation pc <jungil.kwon@sk.com>
2020-12-03 10:51:04 +0900

    add comment

--- lg_fota.py
+++ lg_fota.py
@@ -1,15 +1,11 @@
-# [20201119] git에소 추가
-# 167 PC에서 commit 추가
-# 개인 PC에서 주석 추가
-
 from lg_fota_get_data_from_mail import lg_fota_get_data
 from lg_fota_send_mail import lg_fota_send_mail
 
 from sendMail import *
 sendMail(title="lg_fota started ", text="")
 
-lg_fota_get_data()
-lg_fota_send_mail()
+lg_fota_get_data()  # Mail 서버 접속하여 LG FOTA 메일 다운로드 및 통계 Data 생성
+lg_fota_send_mail() # 그래프 생성하여 메일 전달
 
 from sendMail import *
 sendMail(title="lg_fota end ", text="")
